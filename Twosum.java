11cd211fc8723909e63f92663c0c53e437df7b40
Saiachyuth421 <saiachyuth2222@gmail.com>
2025-05-13 19:58:17 +0530

    anjalisai

---
+++ Twosum.java
@@ -0,0 +1,30 @@
+public class Twosum {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Sample input
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = solution.twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No pair found.");
+        }
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {}; // If no pair found
+    }
+}
